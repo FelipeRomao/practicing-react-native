eb261bc1f8c69f2740ed9f629e1862eefd54862d
Felipe Romao <feliperomao.a@gmail.com>
2019-05-01 00:02:45 -0300

    Inserindo usuários com Authentication do Firebase

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 import firebase from 'firebase'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list : []
+      email : '',
+      senha : ''
     }
 
     let config = {
@@ -17,37 +18,49 @@ export default class App extends Component {
       storageBucket: "",
       messagingSenderId: ""
     };
+
     firebase.initializeApp(config);
 
-    firebase.database().ref('usuarios').on('value', (snapshot) => {
-      let state = this.state
-      state.list= []
-      snapshot.forEach((childItem) => {
-        state.list.push({
-          key : childItem.key,
-          nome : childItem.val().nome,
-          idade : childItem.val().idade
-        })
-      })
-
-      this.setState(state)
-
-    })
+    this.cadastrarUsuario = this.cadastrarUsuario.bind(this)
   }
   
+  cadastrarUsuario() {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.senha)
+    .catch((error) => {
+      switch(error.code) {
+        case 'auth/weak-password' :
+          alert('Sua senha deve ter pelo menos 6 caracteres')
+        break
+
+        case 'auth/invalid-email' : 
+          alert('E-mail inválido! Insira um e-mail válido para seguir com o cadastro do seu usuário')
+        break
+
+        case 'auth/email-already-in-use' :
+          alert('E-mail já sendo utilizado por algum usuário')
+        break
+        
+        default :
+          alert('Ocorreu um erro! Tente novamente mais tarde.')
+        break  
+
+      }
+
+    })
+
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <FlatList 
-          data={this.state.list}
-          renderItem={({item}) => {
-            return(
-              <View>
-                <Text>{item.key} | {item.nome}, {item.idade} anos</Text>
-              </View>
-            )
-          }}
-        />
+        <Text style={styles.text}>E-mail:</Text>
+        <TextInput style={styles.input} onChangeText={(email) => this.setState({email})} />
+
+        <Text style={styles.text}>Senha:</Text>
+        <TextInput style={styles.input} onChangeText={(senha) => this.setState({senha})} 
+        secureTextEntry = {true} />
+
+        <Button title='Cadastrar usuário' onPress={this.cadastrarUsuario} />
       </View>
     )
   }
@@ -58,6 +71,22 @@ const styles = StyleSheet.create({
     flex : 1,
     justifyContent : 'center',
     alignItems : 'center'
+  },
+
+  input : {
+    width : 300,
+    height : 45,
+    borderWidth : 2,
+    borderRadius : 5,
+    borderColor : '#1E90FF',
+    marginBottom : 20
+  },
+
+  text : {
+    fontSize : 16,
+    fontWeight : 'bold',
+    marginLeft : -250 ,
+    marginBottom : 5
   }
 
 });
